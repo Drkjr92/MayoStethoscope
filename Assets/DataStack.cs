6b89441b5b02d97485c151c98c2a71f1a6cb4620
Kenneth Wilcox <wilcox-kim@Kenneths-iMac.local>
2019-10-18 01:59:02 -0400

    Script updated, hierarchy organized

---
+++ Assets/DataStack.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataStack : MonoBehaviour
+{
+
+    /*THIS CLASS WILL CONSIST OF ALL GAMEOBJECTS AND AUDIO FILES
+    CAN USE THIS CLASS TO SEARCH AND SET AUDIO TO GAMEOBJECTS*/
+
+    AudioClip[] audioList;
+    GameObject[] bodyObjects;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gets all audio files in Resources/Audio folder
+        audioList = Resources.LoadAll<AudioClip>("Audio/");
+
+        //gets all GameObjects in scene
+        bodyObjects = GameObject.FindGameObjectsWithTag("Bodypart");
+        foreach(GameObject gos in bodyObjects)
+        {
+            Debug.Log(gos.name);
+        }
+
+    }
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scenes/Scripts/BodyCollider.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*   trying to find methods where this Script can make itself unique. 
+     that way I won't need to hard code each sound and body location.
+     code one place, Let it be usuable & scalable ^ . ^       */
+
+
+public class BodyCollider : MonoBehaviour
+{
+    /************************ DECLARATIONS **************************************************/
+    public AudioClip sound;
+    public string soundContainer;
+    //returns Audiosource component of the GameObject
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+
+
+
+    /**************************************************************************************/
+
+
+    private void Start()
+    { 
+    
+        gameObject.AddComponent<AudioSource>();
+        sound = gameObject.GetComponent<DataStack>().SetAudio(soundContainer);
+        source.clip = sound;
+        source.playOnAwake = false;
+
+    }
+
+
+ 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Construct a ray from the current touch coordinates
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Hit detected");
+                PlaySound(sound);
+            }
+        }
+     }
+
+
+
+    void PlaySound(AudioClip youHealthyBro)
+    {
+        source.PlayOneShot(youHealthyBro);
+    }
+
+
+
+}
+
+
